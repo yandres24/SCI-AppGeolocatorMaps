f2e22e8360dc617f73d4c0c084b0e77a8724e145
MiguelChaparro <mychaparrose@misena.edu.co>
2015-06-30 09:33:28 -0500

    Yesid Changes

--- app/src/main/java/com/sci/www/sci_appgeolocator/MainActivity.java
+++ app/src/main/java/com/sci/www/sci_appgeolocator/MainActivity.java
@@ -1,13 +1,30 @@
 package com.sci.www.sci_appgeolocator;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -27,9 +44,120 @@ public class MainActivity extends ActionBarActivity {
 
         TextView tVBienvenidaService= (TextView) findViewById(R.id.tVBienvenidaService);
         tVBienvenidaService.setTypeface(fontRobo);
+        new RestOperation().execute(UrlRepository.URL_ImeiIsExist, deviceId);
+    }
+    private class RestOperation  extends AsyncTask<String,Void,Void> {
+        String content;
+        String error;
+        String data="";
+        AlertDialog.Builder alerBuilder= new AlertDialog.Builder(MainActivity.this);
+        ProgressBar pbInicial=(ProgressBar) findViewById(R.id.pbInicial);
+        protected Void doInBackground(String...params){
+            BufferedReader bf=null;
+            try {
+                HttpClient httpClient=new DefaultHttpClient();
+                HttpGet httpGet=new HttpGet(params[0]+"?imeiDispositive="+params[1]);
+                try
+                {
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    InputStream inputStream=httpResponse.getEntity().getContent();
+                    bf= new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuffer sb=new StringBuffer();
+                    String line=null;
+                    while ((line=bf.readLine())!=null){
+                        System.out.println(line);
+                        sb.append(line);
+                        sb.append(System.getProperty("line.separator"));
+
+                    }
+                    content=sb.toString();
+                }
+                catch(Exception ex)
+                {
+                    alerBuilder.setMessage(ex.getMessage());
+                    alerBuilder.show();
+                }
+                HttpResponse httpResponse= httpClient.execute(httpGet);
+                InputStream inputStream=httpResponse.getEntity().getContent();
+                bf= new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer sb=new StringBuffer();
+                String line=null;
+                while ((line=bf.readLine())!=null){
+                    System.out.println(line);
+                    sb.append(line);
+                    sb.append(System.getProperty("line.separator"));
+
+                }
+                content=sb.toString();
+            } catch (MalformedURLException ex) {
+                error=ex.getMessage();
+                System.out.println(error);
+            } catch (IOException ex) {
+                error=ex.getMessage();
+                System.out.println(error);
+            }finally {
+                try {
+                    bf.close();
+                } catch (IOException ex) {
+                    System.out.println(error);
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (error!=null){
+                alerBuilder.setMessage(error);
+                alerBuilder.show();
+            }else{
+                Boolean state;
+                String descripcionError;
+                JSONObject jsonObject;
+
+                try {
+                    jsonObject =new JSONObject(content);
+                    state=jsonObject.getBoolean("State");
+                    descripcionError=jsonObject.getString("DescripcionError");
+                    if (state)
+                    {
+                        redirection(true);
+                    }
+                    else{
+                        System.out.println(descripcionError);
+                        if(descripcionError.equals("Ninguno"))
+                        {
+                            redirection(false);
+                        }else{
+                            alerBuilder.setMessage("Se ha Presentado un Error: "+descripcionError);
+                            alerBuilder.show();
+                        }
+
+                    }
+                } catch (JSONException ex) {
+                    System.out.println(ex.getMessage());
+                    alerBuilder.setMessage(ex.getMessage());
+                    alerBuilder.show();
+                }
+
+            }
+        }
 
     }
+    private void redirection(boolean IsRegister){
+        if (IsRegister)
+        {
+            startActivity(new Intent(this, HomeActivity.class));
+        }
+        else{
+            startActivity(new Intent(this, RegisterActivity.class));
+        }
 
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/sci/www/sci_appgeolocator/UrlRepository.java
@@ -0,0 +1,8 @@
+package com.sci.www.sci_appgeolocator;
+
+/**
+ * Created by SCI-DESARROLLO on 30/06/2015.
+ */
+public  class  UrlRepository {
+    public  static  final String   URL_ImeiIsExist="http://186.147.35.26:8082/DeveloperServices/api/Imei/IsExist";
+}
