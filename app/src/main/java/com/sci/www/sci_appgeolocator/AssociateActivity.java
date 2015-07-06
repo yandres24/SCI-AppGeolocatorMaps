a229a99124d9543a8508d6733e3a30592a52ddc2
Andres Merchan <yandres24@hotmail.com>
2015-07-06 15:06:29 -0500

    Andres

--- app/src/main/java/com/sci/www/sci_appgeolocator/AssociateActivity.java
+++
@@ -1,7 +0,0 @@
-package com.sci.www.sci_appgeolocator;
-
-/**
- * Created by Administrador on 02/07/15.
- */
-public class AssociateActivity {
-}
---
+++ app/src/main/java/com/sci/www/sci_appgeolocator/Services/MappingService.java
@@ -0,0 +1,7 @@
+package com.sci.www.sci_appgeolocator.Services;
+
+/**
+ * Created by Administrador on 06/07/15.
+ */
+public class MappingService {
+}
